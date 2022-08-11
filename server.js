955110d7d956820647b84d04972dbd7d97fefcdc
unknown <marcosfvizio@gmail.com>
2022-08-11 20:23:28 -0300

    Desafio manejo de plantillas - Handlebars

--- server.js
+++ server.js
@@ -1,48 +1,63 @@
-import express from "express";
-
+import express from 'express';
+import path from 'path';
+import Handlebars from 'express-handlebars'
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import Contenedor from './Contenedor.js';
 const productos = new Contenedor('./productos.json')
 
 const app = express()
-const port = 4000 || process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+const port = 4000 || process.env.PORT
 
+app.engine(
+    'hbs', 
+    Handlebars.engine({
+        extname: '.hbs', 
+        defaultLayout: 'main.hbs' ,          
+        layoutsDir: path.join(__dirname, 'views'),
+        partialsDir: path.join( __dirname, 'views', 'partials')
+    })
+)
+
+app.set('view engine', 'hbs')
 app.set('views', './views')
-app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
 
-
-
-app.get('/', (req, res) => {  
-  res.render('pages/creador.ejs', {
-    titulo: "Subir productos Risk Store",
-    nav:"creador"})
+app.get('/', async (req, res) => {
+  res.render('layout/crear.hbs',{
+    titulo: "Subir Producto Risk Store",
+    producto: false
+  })
 })
 
+
+app.get('/productos', async (req, res) => {
+    const producto = await productos.getAll();
+    const listExist = producto.length > 0;
+    res.render('layout/index.hbs', {
+        titulo: "Risk Store 2022", 
+        list: producto,
+        listExist,
+        producto: true 
+      })
+})
 app.post('/creador', async (req, res) => {
     const producto = await productos.save(req.body);
     const creado =  producto != -1
     console.log(producto)
-    res.render('pages/creadorConfirmar.ejs', {     
+    res.render('layout/creadorConfirmar.hbs', {     
       hayProducto: creado,
       titulo: 'Creacion de producto'
     })
  })
 
-app.get('/productos', async (req, res) => {
-    const producto = await productos.getAll();
-    const hayLista = producto.length > 0;
-    res.render('pages/productos.ejs', {
-        titulo: "Risk Store", 
-        listaProductos: producto,
-        hayLista,
-        nav:"productos"
-      })
-})
- 
+
+
+
 app.listen(4000, err => {
     if(err) throw new Error(`Erron on server: ${err}`)
     console.log(`Server is running on port ${port}`)
